34fba45299cfc49f443ec753f63d6493f65fc06a
raveenamanghnani <raveena.manghnani@rapidops.com>
2024-11-29 08:54:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebasecloudmessagingdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.firebasecloudmessagingdemo;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(
+                task ->{
+                    String token = task.getResult();
+                    Log.d("FCM Token",token);
+                }
+        );
+    }
+}
